3e5aa20d1ad6cf31eea7783b264226f58efd2fab
duythnh <duytle02@gmail.com>
2024-09-30 11:54:31 +0700

    Board now avaiable to draw, also add eraser

--- main.js
+++ main.js
@@ -1,12 +1,66 @@
-function createBoard() {
-  let board = document.querySelector("#board");
-  board.style.gridTemplateColumns = "repeat (16, 1fr)";
-  board.style.gridTemplateRows = "repeat (16, 1fr)";
+let btn_small = document.querySelector(".btn_small");
+let btn_medium = document.querySelector(".btn_medium");
+let btn_large = document.querySelector(".btn_large");
+let btn_extra_large = document.querySelector(".btn_extra_large");
 
-  for (let i = 0; i < 16*16; i++) {
+let size = 16;
+
+let color = "black";
+
+let btn_eraser = document.querySelector(".btn_eraser");
+let btn_clear = document.querySelector(".btn_clear");
+
+function createBoard(size) {
+  let numGrid = size * size;
+
+  let board = document.querySelector("#board");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  board.innerHTML = "";
+
+  for (let i = 0; i < numGrid; i++) {
     let grid = document.createElement("div");
-    grid.style.backgroundColor = "white";
+    grid.style.border = "1px solid black";
+    grid.style.backgroundColor = "#fff";
+    grid.addEventListener("mouseover", colorSelector);
+
     board.insertAdjacentElement("beforeend", grid);
   }
 }
-createBoard();
+
+function colorSelector() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else if (color === "black") {
+    this.style.backgroundColor = "black";
+  } else if (color === "white") {
+    this.style.backgroundColor = "white";
+  }
+}
+
+function colorSetter(colorSelection) {
+  color = colorSelection;
+}
+
+createBoard(size);
+
+btn_small.addEventListener("click", function () {
+  createBoard(8);
+});
+
+btn_medium.addEventListener("click", function () {
+  createBoard(16);
+});
+
+btn_large.addEventListener("click", function () {
+  createBoard(32);
+});
+
+btn_extra_large.addEventListener("click", function () {
+  createBoard(64);
+});
+
+btn_eraser.addEventListener("click", function () {
+  colorSetter("white");
+});
